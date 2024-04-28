9bbdc3f4816f4eb437052a3bab87db2c8fa0995a
AbdullahAlMaksud <maksud.workspace@gmail.com>
2024-04-28 12:19:46 +0600

    Data get by id Added

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 console.log(port)
 
 //------------------------------
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.1fo0aid.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri, {
   serverApi: {
@@ -27,17 +27,31 @@ async function run() {
 
     const craftItemCollection = client.db('carftItemDB').collection('craftItems')
 
-    app.get('/craftItems', async (req, res)=>{
-        const cursor = craftItemCollection.find()
-        const result = await cursor.toArray()
-        res.send(result)
+    app.get('/craftItems', async (req, res) => {
+      const cursor = craftItemCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
     })
 
-    app.post('/craftItems', async (req, res)=>{
-        const newCraftItem = req.body;
-        const result = await craftItemCollection.insertOne(newCraftItem);
-        res.send(result)
-        console.log(newCraftItem)
+    app.get('/craftItems/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await craftItemCollection.findOne(query);
+      res.send(result)
+    })
+
+    // app.get('/craftItems/:email', async (req, res) => {
+    //   const email = req.params.email;
+    //   const query = { user_email: new ObjectId(email) }
+    //   const cursor = await craftItemCollection.find(query).toArray()
+    //   res.send(cursor)
+    // })
+
+    app.post('/craftItems', async (req, res) => {
+      const newCraftItem = req.body;
+      const result = await craftItemCollection.insertOne(newCraftItem);
+      res.send(result)
+      console.log(newCraftItem)
     })
 
 
@@ -56,10 +70,10 @@ run().catch(console.dir);
 //--------------------
 
 app.get('/', (req, res) => {
-    res.send('ClayNest Server is Running....')
+  res.send('ClayNest Server is Running....')
 })
 
 app.listen(port, () => {
-    console.log(process.env.PORT)
-    console.log(`ClayNest server running on port ${port}`)
+  console.log(process.env.PORT)
+  console.log(`ClayNest server running on port ${port}`)
 })
